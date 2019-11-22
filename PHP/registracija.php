9ee99e9a0c5c1c9f7de61bb0b9caddbd74fde7ee
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-11-22 15:28:03 +0100

    #6 u register.php dodana provjera jesu li došli svi podaci
    Ako su POST zahtjevom došli svi potrebni podaci, tada skripta nastavlja. Ako nisu, onda skripta šalje odgovor da neki parametar nije dobar.

--- PHP/registracija.php
+++ PHP/registracija.php
@@ -5,4 +5,13 @@ $db = new DB_Functions();
 
 require_once 'responseTemplate.php';
         
+$registerCheck=$db->checkRegister($_POST);
+
+if ($registerCheck!==1){
+    $response->STATUS=400;
+    $response->STATUSMESSAGE="BAD REQUEST: BAD PARAMETER: ".$registerCheck;
+    $response = json_encode($response);
+    echo $response;
+    return;
+}
 ?>
