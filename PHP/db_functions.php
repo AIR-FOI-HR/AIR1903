c4282a6fb2d1d6253c0e21a9769a5ce3df63ff2f
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-11-22 15:00:01 +0100

    #6 U db_functions.php dodana funkcija checkRegister
    Funkcija checkRegister provjerava jesu li došli svi podaci potrebni za registraciju

--- PHP/db_functions.php
+++ PHP/db_functions.php
@@ -12,5 +12,14 @@ class DB_Functions {
 	function __destruct() {
 		
 	}
+	
+	public function checkRegister($post){
+        if(!isset($post["Ime"])|| ctype_space($post["Ime"]) || empty($post["Ime"])) return "Ime";
+        if(!isset($post["Prezime"])|| ctype_space($post["Prezime"]) || empty($post["Prezime"])) return "Prezime";
+        if(!isset($post["Email"])|| ctype_space($post["Email"]) || empty($post["Email"])) return "Email";
+        if(!isset($post["KorisnickoIme"])|| ctype_space($post["KorisnickoIme"]) || empty($post["KorisnickoIme"])) return "KorisnickoIme";
+        if(!isset($post["Lozinka"])|| ctype_space($post["Lozinka"]) || empty($post["Lozinka"])) return "Lozinka";
+        return 1;
+    }
 }
 ?>
