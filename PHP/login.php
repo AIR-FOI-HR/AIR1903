dea3fade749133dab43b08b01e2d5fefadca61d3
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-11-22 15:37:49 +0100

    #6 u login.php dodana provjera jesu li došli svi podaci
    Ako su POST zahtjevom došli svi potrebni podaci, tada skripta nastavlja. Ako nisu, onda skripta šalje odgovor da neki parametar nije dobar.

--- PHP/login.php
+++ PHP/login.php
@@ -3,4 +3,14 @@ require_once 'db_function.php';
 $db = new DB_Functions();
 
 require_once 'responseTemplate.php';
+
+$loginCheck=$db->checkLogin($_POST);
+
+if ($loginCheck!==1){
+    $response->STATUS=400;
+    $response->STATUSMESSAGE="BAD REQUEST: BAD PARAMETER: ".$loginCheck;
+    $response = json_encode($response);
+    echo $response;
+    return;
+}
 ?>
