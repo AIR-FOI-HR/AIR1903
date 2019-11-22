0428370fe0054311a08a39e64a17ad0b0d581caf
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-11-22 15:09:11 +0100

    #6 U db_functions.php dodana funkcija checkPassword
    Funkcija checkPassword provjerava podudara li se dobivena lozinka s lozinkom u bazi, tako da lozinkom i saltom napravi hash i usporedi podudara li se hash s hashom u bazi.
    Ako je lozinka ispravna, funkcija vraća JSON objekt u kojem pišu svi podaci o korisniku, osim podataka o lozinki, i UNIX vrijeme prijave.

--- PHP/db_functions.php
+++ PHP/db_functions.php
@@ -39,5 +39,42 @@ class DB_Functions {
         //echo $hashB64."\n";
         return [$saltB64,$hashB64];
     }
+
+    public function checkPassword($post) {
+        $q="SELECT LozinkaSalt, LozinkaHash FROM Korisnik WHERE KorisnickoIme='{$post["KorisnickoIme"]}'";
+        $stmt = $this->conn->query($q);
+        $stmt = $stmt->fetch_assoc();
+        
+        $iterations = 10000;
+        
+        $salt = base64_decode($stmt["LozinkaSalt"]);
+        $hashDB = $stmt["LozinkaHash"];
+        
+        $hash = hash_pbkdf2("sha256", $post["Lozinka"], $salt, $iterations);
+        $hash= base64_encode(pack('H*',$hash));
+        
+        if($hash==$hashDB){
+            $q = "SELECT k.Ime, k.Prezime, k.Email, k.KorisnickoIme, k.StanjeRacuna, k.DozvolaUpravljanjeUlogama, k.DozvolaUpravljanjeStanjemRacuna, k.DozvolaPregledTransakcija, k.DozvolaUvidUStatistiku, k.Id_Uloge, u.Naziv FROM Korisnik k JOIN Uloga u ON (k.Id_Uloge=u.Id) WHERE KorisnickoIme='{$post["KorisnickoIme"]}'";
+            $stmt = $this->conn->query($q);
+            $stmt = $stmt->fetch_assoc();
+            $response["Ime"] = $stmt["Ime"];
+            $response["Prezime"] = $stmt["Prezime"];
+            $response["Email"] = $stmt["Email"];
+            $response["KorisnickoIme"] = $stmt["KorisnickoIme"];
+            $response["StanjeRacuna"] = $stmt["StanjeRacuna"];
+            $response["DozvolaUpravljanjeUlogama"] = $stmt["DozvolaUpravljanjeUlogama"];
+            $response["DozvolaUpravljanjeStanjemRacuna"] = $stmt["DozvolaUpravljanjeStanjemRacuna"];
+            $response["DozvolaPregledTransakcija"] = $stmt["DozvolaPregledTransakcija"];
+            $response["DozvolaUvidUStatistiku"] = $stmt["DozvolaUvidUStatistiku"];
+            $response["Id_Uloge"] = $stmt["Id_Uloge"];
+            $response["Naziv_Uloge"] = $stmt["Naziv"];
+			$response["LoginTime"] = time();
+            
+            $response = json_encode($response);
+            return $response;
+        }
+        else return 0;
+        
+    }
 }
 ?>
