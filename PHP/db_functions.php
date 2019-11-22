28bb68e3eff0e0f1e1edb743d50ad4b6c1312cb1
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-11-22 14:51:43 +0100

    #6 U skriptu db_functions.php dodan konstruktor
    Konstruktor uspostavlja vezu s bazom, kako bi klasa mogla raditi s bazom

--- PHP/db_functions.php
+++ PHP/db_functions.php
@@ -1,6 +1,16 @@
 <?php
 
 class DB_Functions {
+	private $conn;
 
+	function __construct() {
+		require_once 'db_connect.php';
+		$db = new DB_CONNECT();
+		$this->conn = $db->connect();
+	}
+
+	function __destruct() {
+		
+	}
 }
 ?>
