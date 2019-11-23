33512f1568802b52ae9fea7d3c9366aa98df2719
Josip Fuduric <josip0000007@gmail.com>
2019-11-23 16:54:12 +0100

    #9 Izmjena funkcije getClient.
    Izmjena funkcije getClient u objektu RetrofitClient, dodana provjera ukoliko je retrofit objekt prazan kreiramo novi retrofit objekt koji će sadržavati naš baseUrl.

--- Android/app/src/main/java/com/example/pop/Response/RetrofitClient.kt
+++ Android/app/src/main/java/com/example/pop/Response/RetrofitClient.kt
@@ -8,7 +8,10 @@ object RetrofitClient {
     private var retrofit: Retrofit? = null
 
     fun getClient(baseUrl: String): Retrofit {
-
+        if (retrofit == null) {
+            retrofit = Retrofit.Builder().baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create()).build()
+        }
         return retrofit!!
     }
 }
