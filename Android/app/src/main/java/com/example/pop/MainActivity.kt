1db9746b2d37daf433dc7d7695c462f1d509a292
Josip Fuduric <josip0000007@gmail.com>
2019-11-23 17:07:50 +0100

    #7 Promjena klase Main activity.
    Dodana varijabla mService, te event listeneri za gumbe registracijaButton te loginButton.

--- Android/app/src/main/java/com/example/pop/MainActivity.kt
+++ Android/app/src/main/java/com/example/pop/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.pop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    internal lateinit var mService:IMyAPI
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mService = Common.api
+
+        registracijaButton.setOnClickListener{startActivity(Intent(this@MainActivity,Registraacija::class.java))}
+        loginButton.setOnClickListener{authenticateUser(KorisnickoIme.text.toString(),Lozinka.text.toString())}
     }
 }
