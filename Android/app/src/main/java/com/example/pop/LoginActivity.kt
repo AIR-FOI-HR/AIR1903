1af8bf7f689b79fab2545da0b77b7b653801fe3d
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-11-23 20:31:38 +0100

    #7 Promijenjena klasa LoginActivity.kt
    Privremeno isključena varijabla mService, te event listeneri za gumbe registracijaButton te loginButton.
    Funcije i klase potrebne za varijablu i event listenere još ne postoje u ovom branchu, pa stvaraju greške.

--- Android/app/src/main/java/com/example/pop/LoginActivity.kt
+++ Android/app/src/main/java/com/example/pop/LoginActivity.kt
@@ -6,15 +6,15 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-    internal lateinit var mService:IMyAPI
+    //internal lateinit var mService:IMyAPI
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        mService = Common.api
+        //mService = Common.api
 
-        layoutLoginButtonRegister.setOnClickListener{startActivity(Intent(this@LoginActivity,Registraacija::class.java))}
-        layoutLoginButtonLogin.setOnClickListener{authenticateUser(layoutLoginInputUsername.text.toString(),layoutLoginInputPassword.text.toString())}
+        //layoutLoginButtonRegister.setOnClickListener{startActivity(Intent(this@LoginActivity,Registraacija::class.java))}
+        //layoutLoginButtonLogin.setOnClickListener{authenticateUser(layoutLoginInputUsername.text.toString(),layoutLoginInputPassword.text.toString())}
     }
 }
