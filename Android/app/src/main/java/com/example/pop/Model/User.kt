b6b0ac5d3ec6a04e39342ef1000c40fb57e99aa7
Josip Fuduric <josip0000007@gmail.com>
2019-11-23 16:01:28 +0100

    #9 Promjena klase User.
    Klasa user popunjena svim potrebnim varijablama (atributima) koje pojedini korisnik ima.

--- Android/app/src/main/java/com/example/pop/Model/User.kt
+++ Android/app/src/main/java/com/example/pop/Model/User.kt
@@ -1,5 +1,17 @@
 package com.example.pop.Model
 
 class User {
+    var Ime:String=""
+    var Prezime:String=""
+    var Email:String=""
+    var KorisnickoIme:String=""
+    var StanjeRacuna:String=""
+    var DozvolaUpravljanjeUlogama:String=""
+    var DozvolaUpravljanjeStanjemRacuna:String=""
+    var DozvolaPregledTransakcija:String=""
+    var DozvolaUvidUStatistiku:String=""
+    var Id_Uloge:Int=0
+    var Naziv_Uloge:String=""
+    var LoginTime:Int=0
 
 }
