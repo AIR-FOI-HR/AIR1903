09ac01ffa6608962d0947ed80369fc4a6ed070b3
josip <jfuduric@foi.hr>
2019-11-23 15:39:47 +0100

    #8 Dodana nova prazna aktivnost Registracija.
    Dodana nova prezna aktivnost Registracija koja će služiti za registraciju u samu aplikaciju.

---
+++ Android/app/src/main/java/com/example/pop/Registracija.kt
@@ -0,0 +1,12 @@
+package com.example.pop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registracija : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registracija)
+    }
+}
