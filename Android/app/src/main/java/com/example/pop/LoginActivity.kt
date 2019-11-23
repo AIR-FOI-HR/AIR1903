dcf3b12e6122e607841733a69f89e4623173ef72
dbaban <dbaban@foi.hr>
2019-11-23 19:28:45 +0100

    #7 Promjena naziva datoteka
    Promjenjeni su nazivi datoteka activity_main.xml u activity_login.xml i MainActivity.kt u LoginActivity.kt
    Početni activity u android manifestu je postavljen na LoginActivity

--- Android/app/src/main/java/com/example/pop/LoginActivity.kt
+++ Android/app/src/main/java/com/example/pop/LoginActivity.kt
@@ -3,18 +3,18 @@ package com.example.pop
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_login.*
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     internal lateinit var mService:IMyAPI
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         mService = Common.api
 
-        layoutLoginButtonRegister.setOnClickListener{startActivity(Intent(this@MainActivity,Registraacija::class.java))}
+        layoutLoginButtonRegister.setOnClickListener{startActivity(Intent(this@LoginActivity,Registraacija::class.java))}
         layoutLoginButtonLogin.setOnClickListener{authenticateUser(layoutLoginInputUsername.text.toString(),layoutLoginInputPassword.text.toString())}
     }
 }
