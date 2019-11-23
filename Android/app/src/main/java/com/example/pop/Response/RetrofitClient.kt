57a6eb31e18fbd3654620b91a1e9c88df6b6eb77
Josip Fuduric <josip0000007@gmail.com>
2019-11-23 16:42:08 +0100

    #9 Promjena objekta RetrofitClient.
    Promjena objekta RetrofitClient, dodana funkcija getClient koja kao parametar prima baseUrl (što je zapravo url direktorija).

--- Android/app/src/main/java/com/example/pop/Response/RetrofitClient.kt
+++ Android/app/src/main/java/com/example/pop/Response/RetrofitClient.kt
@@ -1,4 +1,14 @@
 package com.example.pop.Response
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitClient {
+
+    private var retrofit: Retrofit? = null
+
+    fun getClient(baseUrl: String): Retrofit {
+
+        return retrofit!!
+    }
 }
