f0cce87933821f4feccb7a38d8aa05ba94e500e2
Josip Fuduric <josip0000007@gmail.com>
2019-11-23 17:00:24 +0100

    #9 Promjena objekta Common.
    Dodane dvije nove konstante BASE_URL, te api.

--- Android/app/src/main/java/com/example/pop/Common/Common.kt
+++ Android/app/src/main/java/com/example/pop/Common/Common.kt
@@ -1,4 +1,12 @@
 package com.example.pop.Common
 
+import com.example.pop.Response.IMyAPI
+import com.example.pop.Response.RetrofitClient
+
 object Common {
+
+    val BASE_URL = "https://cortex.foi.hr/pop/"
+
+    val api: IMyAPI
+        get() = RetrofitClient.getClient(BASE_URL).create(IMyAPI::class.java)
 }
