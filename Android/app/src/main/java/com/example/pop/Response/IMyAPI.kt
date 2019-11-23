996ad573e7c7b045d201cd58c214a1d179468029
Josip Fuduric <josip0000007@gmail.com>
2019-11-23 16:29:50 +0100

    #9 Izmjena u interface-u IMyAPI.
    Dodana funkcijja registerUser koja služi za prosljeđivanje svih potrebnih parametara php skripti registracija.php.

--- Android/app/src/main/java/com/example/pop/Response/IMyAPI.kt
+++ Android/app/src/main/java/com/example/pop/Response/IMyAPI.kt
@@ -1,4 +1,16 @@
 package com.example.pop.Response
 
+import com.example.pop.Model.ApiResponse
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface IMyAPI {
+    @FormUrlEncoded
+
+    @POST("registracija.php")
+    fun registerUser(@Field("Ime") Ime:String, @Field("Prezime") Prezime:String, @Field("LozinkaTest") LozinkaTest:String, @Field("Email") Email:String, @Field("KorisnickoIme") KorisnickoIme:String): Call<ApiResponse>
+
+
 }
