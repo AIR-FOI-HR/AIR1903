6c8d8988882c141490e386a3a13bb58ac274e1e6
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-11-24 16:37:07 +0100

    #8 Na drugi fragment registracije dodan složeniji OnClickListener
    Listener provjerava jesu li lozinka i provjera lozinke iste, te jesu li sva polja unosa ispunjena.
    Privremeno isključen prelaz na treći fragment

--- Android/app/src/main/java/com/example/pop/RegistrationSecond.kt
+++ Android/app/src/main/java/com/example/pop/RegistrationSecond.kt
@@ -5,10 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_registration_second.view.*
+class RegistrationSecond : Fragment(), View.OnClickListener {
 
-class RegistrationSecond : Fragment() {
+    override fun onClick(v: View?) {
+        if (!checkPassword(v?.layoutRegistrationInputPassword?.text.toString(),v?.layoutRegistrationInputPasswordConfirm?.text.toString())){
+            Toast.makeText(activity,"Lozinke se ne podudaraju",Toast.LENGTH_SHORT).show()
+        }
+        else{
+            RegistrationData.Lozinka = v?.layoutRegistrationInputPassword?.text.toString()
+            if (RegistrationData.Lozinka==""){
+                Toast.makeText(activity,"Lozinka ne smije biti prazna",Toast.LENGTH_SHORT).show()
+                return
+            }
+            RegistrationData.Email = v?.layoutRegistrationInputEmail?.text.toString()
+            if (RegistrationData.Email==""){
+                Toast.makeText(activity,"Email ne smije biti prazan",Toast.LENGTH_SHORT).show()
+                return
+            }
+            RegistrationData.KorisnickoIme = v?.layoutRegistrationInputUsername?.text.toString()
+            if (RegistrationData.KorisnickoIme==""){
+                Toast.makeText(activity,"Korisničko ime ne smije biti prazno",Toast.LENGTH_SHORT).show()
+                return
+            }
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,8 +39,18 @@ class RegistrationSecond : Fragment() {
     ): View? {
         val view: View = inflater.inflate(R.layout.fragment_registration_second, container, false)
         view.layoutRegistrationButtonRegister.setOnClickListener {
-            it.findNavController().navigate(R.id.action_registrationSecond_to_registrationThird)
+            onClick(view)
         }
         return view
+    }
+
+    private fun checkPassword(
+        Lozinka: String,
+        PotvrdaLozinke: String
+    ):Boolean{
+        if (Lozinka==PotvrdaLozinke) {
+            return true
+        }
+        return false
     }
 }
