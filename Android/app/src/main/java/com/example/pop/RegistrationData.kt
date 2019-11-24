1edbb4910eb7c4c7d44d4fc5251e123db11970db
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-11-24 16:23:52 +0100

    #8 Dodan objekt RegistrationData
    U objekt će se čuvati podaci za registraciju pri tranziciji između fragmenata

---
+++ Android/app/src/main/java/com/example/pop/RegistrationData.kt
@@ -0,0 +1,18 @@
+package com.example.pop
+
+object RegistrationData {
+    var Ime:String=""
+    var Prezime:String=""
+    var KorisnickoIme:String=""
+    var Email:String=""
+    var Lozinka:String=""
+
+    fun Reset(){
+        Ime=""
+        Prezime=""
+        KorisnickoIme=""
+        Email=""
+        Lozinka=""
+    }
+
+}
