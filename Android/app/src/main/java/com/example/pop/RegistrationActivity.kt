b7109c2ee32688c2e39c8ebd737b81e5adf12eae
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-11-24 16:27:52 +0100

    #7, #8 Na aktivnost na registraciju dodan onClickListener
    Klikom na "Already have an account?" aplikacija otvara aktivnost prijave

--- Android/app/src/main/java/com/example/pop/RegistrationActivity.kt
+++ Android/app/src/main/java/com/example/pop/RegistrationActivity.kt
@@ -1,12 +1,22 @@
 package com.example.pop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        layoutRegistrationTextLogin.setOnClickListener {
+            var intent = Intent(this@RegistrationActivity,LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent);
+            RegistrationData.Reset()
+            this@RegistrationActivity!!.finish()
+        }
     }
 }
