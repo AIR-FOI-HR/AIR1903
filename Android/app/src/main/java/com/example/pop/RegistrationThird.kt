f5e844adcb1a1919910742b0ca363eff9341f422
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-11-24 16:32:55 +0100

    #8 Na treći fragment registracije dodan OnClickListener
    Klikom na gumb Login otvara se aktivnost za prijavu

--- Android/app/src/main/java/com/example/pop/RegistrationThird.kt
+++ Android/app/src/main/java/com/example/pop/RegistrationThird.kt
@@ -1,18 +1,30 @@
 package com.example.pop
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat.finishAffinity
+import kotlinx.android.synthetic.main.fragment_registration_third.view.*
 
 class RegistrationThird : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_registration_third, container, false)
+        val view:View = inflater.inflate(R.layout.fragment_registration_third, container, false)
+        view.button.setOnClickListener {
+            var intent = Intent(activity,LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent);
+            RegistrationData.Reset()
+            activity!!.finish()
+        }
+
+        return view
     }
 }
