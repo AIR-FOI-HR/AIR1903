cc1ca11b38d939a2257d5361df2876d27e48eafd
dbaban <dbaban@foi.hr>
2019-11-24 09:07:50 +0100

    #8 Dodan kod za navigaciju između fragmenata
    Napisan je kod za prijelaz između prvoga i drugoga fragmentra registracije

--- Android/app/src/main/java/com/example/pop/RegistrationFirst.kt
+++ Android/app/src/main/java/com/example/pop/RegistrationFirst.kt
@@ -7,13 +7,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_registration_first.view.*
 
 class RegistrationFirst : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration_first, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_registration_first, container, false)
+        view.layoutRegistrationButtonNext.setOnClickListener {
+            it.findNavController().navigate(R.id.action_registrationFirst_to_registrationSecond)
+        }
+        return view
     }
 }
--- Android/app/src/main/java/com/example/pop/RegistrationSecond.kt
+++ Android/app/src/main/java/com/example/pop/RegistrationSecond.kt
@@ -7,13 +7,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_registration_second.view.*
 
 class RegistrationSecond : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration_second, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_registration_second, container, false)
+        view.layoutRegistrationButtonRegister.setOnClickListener {
+            //it.findNavController().navigate()
+        }
+        return view
     }
 }
