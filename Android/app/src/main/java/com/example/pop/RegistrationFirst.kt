5eda12bb76a3b5529635ce8c76d3cd70133e869b
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-11-24 16:31:25 +0100

    #8 Dodan kompleksniji OnClickListener
    Novi listener ne dopušta prelazak na drugi fragment ako polja za unos imena i prezimena nisu ispunjena

--- Android/app/src/main/java/com/example/pop/RegistrationFirst.kt
+++ Android/app/src/main/java/com/example/pop/RegistrationFirst.kt
@@ -5,10 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_registration_first.view.*
 
-class RegistrationFirst : Fragment() {
+class RegistrationFirst : Fragment(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        RegistrationData.Ime = v?.layoutRegistrationInputName?.text.toString()
+        if (RegistrationData.Ime == ""){
+            Toast.makeText(activity,"Ime ne smije biti prazno", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        RegistrationData.Prezime = v?.layoutRegistrationInputSurname?.text.toString()
+        if (RegistrationData.Prezime==""){
+            Toast.makeText(activity,"Prezime ne smije biti prazno", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        v!!.findNavController().navigate(R.id.action_registrationFirst_to_registrationSecond)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,7 +33,7 @@ class RegistrationFirst : Fragment() {
     ): View? {
         val view: View = inflater.inflate(R.layout.fragment_registration_first, container, false)
         view.layoutRegistrationButtonNext.setOnClickListener {
-            it.findNavController().navigate(R.id.action_registrationFirst_to_registrationSecond)
+            onClick(view)
         }
         return view
     }
