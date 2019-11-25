19523c3d306e49e3ffe0ae3db057facb5ee54b15
dbaban <dbaban@foi.hr>
2019-11-25 04:09:50 +0100

    #8 Dodana funkcionalnost swipe mogućnosti za treći fragment
    Na trećem fragmentu swipe gesta u bilo kojem smjeru vraća korisnika na login zaslon

--- Android/app/src/main/java/com/example/pop/RegistrationFirst.kt
+++ Android/app/src/main/java/com/example/pop/RegistrationFirst.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_registration_first.view.*
 import kotlin.math.abs
 
--- Android/app/src/main/java/com/example/pop/RegistrationThird.kt
+++ Android/app/src/main/java/com/example/pop/RegistrationThird.kt
@@ -2,14 +2,14 @@ package com.example.pop
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat.finishAffinity
 import kotlinx.android.synthetic.main.fragment_registration_third.view.*
+import kotlin.math.abs
 
 class RegistrationThird : Fragment() {
     override fun onCreateView(
@@ -18,13 +18,22 @@ class RegistrationThird : Fragment() {
     ): View? {
         val view:View = inflater.inflate(R.layout.fragment_registration_third, container, false)
         view.button.setOnClickListener {
-            var intent = Intent(activity,LoginActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent);
-            RegistrationData.Reset()
-            activity!!.finish()
+            startLoginActivity()
         }
-
+        view.setOnTouchListener { _, event : MotionEvent ->
+            if (event.action == MotionEvent.ACTION_DOWN) touchX = event.x
+            if (event.action == MotionEvent.ACTION_UP)
+                if(abs(touchX - event.x) > SWIPE_THRESHOLD) startLoginActivity()
+            true
+        }
         return view
+    }
+
+    private fun startLoginActivity() {
+        val intent = Intent(activity,LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent);
+        RegistrationData.Reset()
+        activity!!.finish()
     }
 }
