5d443b444a9dc5154843ee94e4c4106f0cafbf1e
Mattheeeew <44161476+Mattheeeew@users.noreply.github.com>
2019-11-28 02:30:23 +0100

    #62 Kreiran adapter, implementacija metode sučelja ProductApi.
    RecyclerView-u dodijeljena lista proizvoda, implementirana metoda za dohvaćanje svih proizvoda sučelja ProductApi.

--- Android/app/src/main/java/com/example/pop/ShowProductsActivity.kt
+++ Android/app/src/main/java/com/example/pop/ShowProductsActivity.kt
@@ -9,7 +9,16 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.database.Entities.Product
 import com.squareup.picasso.Picasso
-
+import com.example.database.Entities.ProductResponse
+import com.example.webservice.Response.ProductApi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_show_products.*
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
 class ShowProductsActivity : AppCompatActivity() {
 
     private lateinit var productAdapter: ProductsAdapter
@@ -17,6 +26,25 @@ class ShowProductsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_products)
+
+        productAdapter = ProductsAdapter()
+        lista_proizvoda.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        lista_proizvoda.adapter = productAdapter
+        val retrofit : Retrofit = Retrofit.Builder()
+            .baseUrl("http://cortex.foi.hr")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+        val productsApi = retrofit.create(ProductApi::class.java)
+
+        productsApi.getProducts()
+            .subscribeOn(Schedulers.io())
+            .unsubscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ productAdapter.setProducts(it.data) },
+                {
+                    Toast.makeText(applicationContext, it.message, Toast.LENGTH_SHORT).show()
+                })
     }
 
 
