64ebc9e21449d78575dd204fbb7bc0dd472550ac
Mattheeeew <44161476+Mattheeeew@users.noreply.github.com>
2019-11-28 02:01:31 +0100

    #62 Kreirana klasa Product sa pripadajućim atributima.
    Dodan modul unutar build.gradle datoteke. Kreirana klasa Product sa atributima Id, Naziv, Cijena, Opis, Slika.

--- Android/app/src/main/java/com/example/pop/ShowProductsActivity.kt
+++ Android/app/src/main/java/com/example/pop/ShowProductsActivity.kt
@@ -2,11 +2,13 @@ package com.example.pop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.database.Entities.Product
 
 class ShowProductsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_products)
+
     }
 }
---
+++ Android/database/src/main/java/com/example/database/Entities/Product.kt
@@ -0,0 +1,9 @@
+package com.example.database.Entities
+
+class Product {
+    var Id : Int = 0
+    var Naziv : String? = null
+    var Cijena : String? = null
+    var Opis : String? = null
+    var Slika : String? = null
+}
