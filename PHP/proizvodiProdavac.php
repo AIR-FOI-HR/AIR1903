393868861486f2558202e6bfcfdf23e6bad1d115
Josip Fuduric <josip0000007@gmail.com>
2019-11-30 14:29:02 +0100

    #59 Izmjena funkcije proizvodiProdavac.php.
    Dodana provjera da li je skripti poslan POST zahtjev, ukoliko jeste, skripta ispisuje proizvode pojedinog prodavača.

--- PHP/proizvodiProdavac.php
+++ PHP/proizvodiProdavac.php
@@ -3,5 +3,11 @@ require_once 'db_function.php';
 $db = new DB_Functions();
 $isSeller = $db->isSeller($_POST);
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $sellerProducts = $db->getSellerProducts($_POST);
+    $sellerProducts = json_encode(array('data' => $sellerProducts));
+    echo ($sellerProducts);
+}
+
 ?>
 
