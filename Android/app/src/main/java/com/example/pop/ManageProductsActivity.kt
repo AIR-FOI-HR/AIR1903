c905f5bd2da257895478ec1a0de290fed5e70d1b
dbaban <dbaban@foi.hr>
2019-12-01 15:17:40 +0100

    #14, #39 Promjena layouta sučelja i dodavanje funkcionalnosti za uređivanje
    Aktivnost se poziva na način da se u intent doda .putExtra("productId", "[id]") ukoliko će aktivnost uređivati postojeći proizvod
    Za dodavanje novog proizvoda, aktivnost se poziva normalno bez putExtra

--- Android/app/src/main/java/com/example/pop/ManageProductsActivity.kt
+++ Android/app/src/main/java/com/example/pop/ManageProductsActivity.kt
@@ -2,12 +2,35 @@ package com.example.pop
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
+import com.example.database.Entities.Product
+import kotlinx.android.synthetic.main.activity_manage_products.*
 
 class ManageProductsActivity : AppCompatActivity() {
+    private lateinit var product : Product
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage_products)
 
+        if(intent.hasExtra("productId")) {
+            layoutManageProductsButtonSubmit.text = getString(R.string.buttonSave)
+            if(getProduct(intent.getIntExtra("productId", -1))) {
+                layoutManageProductsInputName.setText(product.Naziv)
+                layoutManageProductsInputValue.setText(product.Cijena)
+                layoutManageProductsImage.setImageURI(product.Slika?.toUri())
+            }
+        }
+        else {
+            layoutManageProductsButtonSubmit.text = getString(R.string.buttonAdd)
+        }
+    }
+
+    private fun getProduct(id : Int) : Boolean{
+        if(id != -1) {
+            //Tu treba popunit varijablu product sa podacima iz baze na temelju id-a
+            return true
+        }
+        return false
     }
 }
