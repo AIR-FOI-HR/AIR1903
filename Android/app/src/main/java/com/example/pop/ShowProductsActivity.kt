89dc593f3784372fe71fea800255f4f14aef08e0
Mattheeeew <44161476+Mattheeeew@users.noreply.github.com>
2019-12-03 18:48:29 +0100

    #62 Promjena aktivnosti za prikaz proizvoda ShowProductsActivity.
    Promjena metoda unutar klase ShowProductsActivity, korišten drugačiji pristup za prikaz proizvoda.

--- Android/app/src/main/java/com/example/pop/ShowProductsActivity.kt
+++ Android/app/src/main/java/com/example/pop/ShowProductsActivity.kt
@@ -2,27 +2,48 @@ package com.example.pop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.pop.adapters.DataSource
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.pop.adapters.ProductRecyclerAdapter
-import com.example.pop.adapters.TopSpacingItemDecoration
+import com.example.webservice.Response.ProductApi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_show_products.*
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class ShowProductsActivity : AppCompatActivity(){
-
-
-    private lateinit var blogAdapter: ProductRecyclerAdapter
+    private lateinit var productAdapter: ProductRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_products)
 
-        initRecyclerView()
-        addDataSet()
+        productAdapter = ProductRecyclerAdapter()
+        lista_proizvoda.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        lista_proizvoda.adapter = productAdapter
+
+        val retrofit : Retrofit = Retrofit.Builder()
+            .baseUrl("http://cortex.foi.hr")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+        val productsApi = retrofit.create(ProductApi::class.java)
+
+        productsApi.getProducts()
+            .subscribeOn(Schedulers.io())
+            .unsubscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ productAdapter.submitList(it.DATA) },
+                {
+                    Toast.makeText(applicationContext, it.message, Toast.LENGTH_SHORT).show()
+                })
     }
 
+    /*
     private fun addDataSet(){
         val data = DataSource.createDataSet()
         blogAdapter.submitList(data)
@@ -38,5 +59,5 @@ class ShowProductsActivity : AppCompatActivity(){
             adapter = blogAdapter
         }
     }
-
+     */
 }
