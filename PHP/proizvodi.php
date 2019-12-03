d234742e37f991f565d7c582e82a9cb7e1215ccd
Josip Fuduric <josip0000007@gmail.com>
2019-12-03 15:08:12 +0100

    #59 Izmjena skripte proizvodi.php.
    Popravljen odgovor prilikom dohvata svih proizvoda.

--- PHP/proizvodi.php
+++ PHP/proizvodi.php
@@ -4,9 +4,12 @@ require_once 'db_function.php';
 $db = new DB_Functions();
 
 if($_SERVER['REQUEST_METHOD'] === 'GET'){
-$allProducts = $db->getAllProducts();
-$allProducts = json_encode(array('data' => $allProducts));
-echo ($allProducts);
+   $allProducts = $db->getAllProducts();
+    $response->DATA = $allProducts;
+    $response->STATUS = true;
+    $response->STATUSMESSAGE = "OK";
+    $response= json_encode($response);
+    echo stripslashes($response);
 }
  else {
   $productCheck = $db->checkProductEmpty($_POST);
