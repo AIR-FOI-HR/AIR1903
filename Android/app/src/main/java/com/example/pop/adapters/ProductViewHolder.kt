7fa8b84f46afcec6861a368a82b7ea8418f8866f
Mattheeeew <44161476+Mattheeeew@users.noreply.github.com>
2019-12-03 18:43:40 +0100

    #62 Korišten drugačiji pristup za prikaz podataka unutar elemenata kartice.
    Promjena klase ProductViewHolder, dodane varijable za dodjeljivanje vrijednosti elementima unutar kartice, promjena metode bind, preimenovani atributi za pristup podacima proizvoda.

--- Android/app/src/main/java/com/example/pop/adapters/ProductViewHolder.kt
+++ Android/app/src/main/java/com/example/pop/adapters/ProductViewHolder.kt
@@ -10,11 +10,19 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.database.Entities.Product
 import com.example.pop.R
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.product_list_item.view.*
+import org.w3c.dom.Text
 
 class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    fun bind(product: Product) { 
+    val productName : TextView = itemView.findViewById(R.id.productName)
+    val productPrice : TextView = itemView.findViewById(R.id.productPrice)
+    val productDescription : TextView = itemView.findViewById(R.id.productDescription)
+    val productImage : ImageView = itemView.findViewById(R.id.productImage)
+
+    fun bind(product: Product) {
+        /*
         val requestOptions = RequestOptions()
             .placeholder(R.drawable.ic_launcher_background)
             .error(R.drawable.ic_launcher_background)
@@ -23,10 +31,17 @@ class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             .applyDefaultRequestOptions(requestOptions)
             .load(product.picture)
             .into(itemView.product_image)
+        */
+        productName.text = product.Naziv
+        productPrice.text = product.Cijena
+        productDescription.text = product.Opis
+        Picasso.get().load(product.Slika).into(productImage)
 
+        /*
         itemView.product_name.text = product.name
         itemView.product_desc.text = product.description
         itemView.product_price.text = product.price.toString()
+         */
     }
 
 }
