a1d16b8762cc94cd8bb53b968583d4abcef2b694
Josip Fuduric <josip0000007@gmail.com>
2019-12-06 11:31:35 +0100

    #16 Izmjena objekta Product.
    Dodana nova klasa NewProduct, koja će nam služiti prilikom dodavanja novog proizvoda.

--- Android/webservice/src/main/java/com/example/webservice/Model/Product.kt
+++ Android/webservice/src/main/java/com/example/webservice/Model/Product.kt
@@ -3,3 +3,9 @@ package com.example.webservice.Model
 class Product(val Id: Int, var Naziv: String, var Cijena: Double, var Opis: String?, var Slika: String){
     var isExpanded: Boolean = false
 }
+class NewProduct{
+    var Naziv:String=""
+    var Cijena:String=""
+    var Opis:String=""
+    var Slika:String=""
+}
