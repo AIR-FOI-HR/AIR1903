ba6292a5ae5a7bded95eb02dac1742588323459f
Josip Fuduric <josip0000007@gmail.com>
2019-12-06 11:43:30 +0100

    #16 Izmjena klase AddNewProduct.
    Dodana nova funkcija addNewProduct, koja će služiti za dodavanje novog proizvoda u bazu podataka.

--- Android/app/src/main/java/com/example/pop/AddNewProduct.kt
+++ Android/app/src/main/java/com/example/pop/AddNewProduct.kt
@@ -2,11 +2,23 @@ package com.example.pop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.webservice.Model.NewProductResponse
+import com.example.webservice.Response.IMyAPI
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class AddNewProduct : AppCompatActivity() {
+    internal lateinit var mService: IMyAPI
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new_product)
+    }
+
+    private fun addNewProduct(Naziv: String, Opis: String, Cijena: String, Slika: String) {
+
     }
 }
