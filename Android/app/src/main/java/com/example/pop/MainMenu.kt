97be2af88cd5dabbea8a9b4695b13ae0e8d22c50
Josip Fuduric <josip0000007@gmail.com>
2019-12-06 11:18:58 +0100

    #15, #16 Izmjena klase MainMenu.
    Dodani OnClick event listenery za oba gumba na aktivnost.

--- Android/app/src/main/java/com/example/pop/MainMenu.kt
+++ Android/app/src/main/java/com/example/pop/MainMenu.kt
@@ -3,12 +3,16 @@ package com.example.pop
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main_menu.*
 
 class MainMenu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        showAllProductsButton.setOnClickListener{showProducts()}
+        addProductButton.setOnClickListener{addNewProduct()}
     }
     private fun showProducts(){
         val intent = Intent(this, ShowProductsActivity::class.java)
