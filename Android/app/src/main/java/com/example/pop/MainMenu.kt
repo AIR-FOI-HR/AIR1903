2fff6a7e751b3a22fe292d46e13ffdf68cab8e3a
Josip Fuduric <josip0000007@gmail.com>
2019-12-06 11:08:29 +0100

    #15, #16 Klasa MainMenu promjenjena u activity.
    Svrha klase jest ista kao i prije.

--- Android/app/src/main/java/com/example/pop/MainMenu.kt
+++ Android/app/src/main/java/com/example/pop/MainMenu.kt
@@ -1,4 +1,12 @@
 package com.example.pop
 
-class MainMenu {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
 }
