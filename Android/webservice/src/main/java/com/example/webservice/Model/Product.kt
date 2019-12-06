520c540325d79df96cab5782688e82f05e97d915
aalagic <aldin_alagic@hotmail.com>
2019-12-06 02:58:27 +0100

    #62 Izmijenjena klasa proizvoda
    Izmijenjena klasa Product.kt

--- Android/webservice/src/main/java/com/example/webservice/Model/Product.kt
+++ Android/webservice/src/main/java/com/example/webservice/Model/Product.kt
@@ -1,3 +1,5 @@
 package com.example.webservice.Model
 
-data class Product(val Id: Int, var Naziv: String?, var Cijena: String?, var Opis: String?, var Slika : String?)
+class Product(val Id: Int, var Naziv: String, var Cijena: Double, var Opis: String?, var Slika: String){
+    var isExpanded: Boolean = false
+}
