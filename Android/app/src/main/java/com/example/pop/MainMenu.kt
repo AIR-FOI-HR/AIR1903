5d2aba9277483bfb149180b7d7cf9576263c9edd
Josip Fuduric <josip0000007@gmail.com>
2019-12-06 12:02:59 +0100

    #15,  #16 Izmjena klase MainMenu.
    Dodani pojedini importovi.

--- Android/app/src/main/java/com/example/pop/MainMenu.kt
+++ Android/app/src/main/java/com/example/pop/MainMenu.kt
@@ -3,13 +3,19 @@ package com.example.pop
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pop.R
+import com.example.webservice.Common.Common
+import com.example.webservice.Response.IMyAPI
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main_menu.*
 
 class MainMenu : AppCompatActivity() {
+    internal lateinit var mService: IMyAPI
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+        mService = Common.api
 
         showAllProductsButton.setOnClickListener{showProducts()}
         addProductButton.setOnClickListener{addNewProduct()}
@@ -22,5 +28,4 @@ class MainMenu : AppCompatActivity() {
         val intent = Intent(this, AddNewProduct::class.java)
         startActivity(intent)
     }
-
 }
