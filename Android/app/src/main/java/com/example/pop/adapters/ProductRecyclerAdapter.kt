6995638c0ae3ea50a15e1a604bd2259e1497ae5f
aalagic <aldin_alagic@hotmail.com>
2019-12-06 03:04:04 +0100

    #62 Dodani "listeneri" na tipke za izmjenu i brisanje proizvoda
    Dodan ispis testnih podataka na klik pogleda img_edit_product i img_delete_product.

--- Android/app/src/main/java/com/example/pop/adapters/ProductRecyclerAdapter.kt
+++ Android/app/src/main/java/com/example/pop/adapters/ProductRecyclerAdapter.kt
@@ -1,22 +1,48 @@
 package com.example.pop.adapters
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.pop.ManageProductsActivity
 import com.example.webservice.Model.Product
 import com.example.pop.R
+import com.example.pop.RegistrationActivity
+import com.example.pop.ShowProductsActivity
+import kotlinx.android.synthetic.main.product_list_item.view.*
+import kotlin.collections.ArrayList
 
+class ProductRecyclerAdapter (val context: Context) : RecyclerView.Adapter<ProductViewHolder>(){
 
-class ProductRecyclerAdapter : RecyclerView.Adapter<ProductViewHolder>()
-{
-    private val products: MutableList<Product> = mutableListOf()
+    private var products: ArrayList<Product> = ArrayList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
         return ProductViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.product_list_item, parent, false))
     }
 
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
-        holder.bind(products[position])
+
+        val product = products[position]
+
+        holder.bind(product)
+
+        holder.itemView.setOnClickListener {
+            val expanded = product.isExpanded
+            product.isExpanded = !expanded
+            notifyItemChanged(position)
+        }
+
+        holder.itemView.img_edit_product.setOnClickListener{displayText("EDIT")}
+        holder.itemView.img_delete_product.setOnClickListener{displayText("DELETE")}
+
+    }
+
+    private fun displayText(text: String) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
 
     override fun getItemCount(): Int {
@@ -27,7 +53,6 @@ class ProductRecyclerAdapter : RecyclerView.Adapter<ProductViewHolder>()
         products.addAll(data)
         notifyDataSetChanged()
     }
-
 
 
 }
