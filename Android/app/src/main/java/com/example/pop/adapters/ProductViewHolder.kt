7c41305578eb1f6869163715290b180d0a497ce0
aalagic <aldin_alagic@hotmail.com>
2019-12-06 03:02:13 +0100

    #62 Dodano sintetičko povezivanje pogleda u ProductViewHolderu
    Dodan novi način povezivanje pogleda (product_name, product_desc...) s vrijednostima proizvoda.

--- Android/app/src/main/java/com/example/pop/adapters/ProductViewHolder.kt
+++ Android/app/src/main/java/com/example/pop/adapters/ProductViewHolder.kt
@@ -1,41 +1,33 @@
 package com.example.pop.adapters
 
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.webservice.Model.Product
+import com.bumptech.glide.request.RequestOptions
 import com.example.pop.R
+import kotlinx.android.synthetic.main.product_list_item.view.*
 import com.squareup.picasso.Picasso
+import com.example.webservice.Model.Product
+
 
 class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    val productName : TextView = itemView.findViewById(R.id.productName)
-    val productPrice : TextView = itemView.findViewById(R.id.productPrice)
-    val productDescription : TextView = itemView.findViewById(R.id.productDescription)
-    val productImage : ImageView = itemView.findViewById(R.id.productImage)
-
     fun bind(product: Product) {
-        /*
+
+        val expanded = product.isExpanded
+        itemView.sub_item.visibility = if (expanded) View.VISIBLE else View.GONE
+
         val requestOptions = RequestOptions()
             .placeholder(R.drawable.ic_launcher_background)
             .error(R.drawable.ic_launcher_background)
-        
-        Glide.with(itemView.context)
+
+        itemView.product_name.text = product.Naziv
+        itemView.product_desc.text = product.Opis
+        itemView.product_price.text = product.Cijena.toString()
+        /*Glide.with(itemView.context)
             .applyDefaultRequestOptions(requestOptions)
             .load(product.picture)
-            .into(itemView.product_image)
-        */
-        productName.text = product.Naziv
-        productPrice.text = product.Cijena
-        productDescription.text = product.Opis
-        Picasso.get().load(product.Slika).into(productImage)
-
-        /*
-        itemView.product_name.text = product.name
-        itemView.product_desc.text = product.description
-        itemView.product_price.text = product.price.toString()
-         */
+            .into(itemView.product_image)*/
+        Picasso.get().load(product.Slika).into(itemView.product_image)
     }
 
 }
