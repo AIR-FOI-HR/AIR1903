9e4c04f285f1e1185ee93c2046dc3825abd5fde6
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-12-06 13:28:06 +0100

    #62 Metoda za dohvat svih proizvoda promijenjena
    Promijenjena metoda iz GET u POST
    getProducts sad treba Readall zastavicu i token

--- Android/webservice/src/main/java/com/example/webservice/Response/IMyAPI.kt
+++ Android/webservice/src/main/java/com/example/webservice/Response/IMyAPI.kt
@@ -12,21 +12,19 @@ import retrofit2.http.POST
 
 interface IMyAPI {
     @FormUrlEncoded
-
     @POST("registracija.php")
     fun registerUser(@Field("Ime") Ime:String, @Field("Prezime") Prezime:String, @Field("Lozinka") Lozinka:String, @Field("Email") Email:String, @Field("KorisnickoIme") KorisnickoIme:String): Call<ApiResponseUser>
 
     @FormUrlEncoded
     @POST("login.php")
-
     fun storeUser(@Field("KorisnickoIme") KorisnickoIme:String, @Field("Lozinka") Lozinka:String):Call<ApiResponseUser>
-
-    @GET("proizvodi.php")
-    fun getProducts() : Call<ProductResponse>
 
     @FormUrlEncoded
     @POST("proizvodi.php")
+    fun getProducts(@Field("Readall") Readall:Boolean, @Field("Token") Token:String) : Call<ProductResponse>
 
+    @FormUrlEncoded
+    @POST("proizvodi.php")
     fun addNewProduct(@Field("Naziv") Naziv:String, @Field("Opis") Opis:String, @Field("Cijena") Cijena:String, @Field("Slika") Slika:String):Call<NewProductResponse>
 
 }
