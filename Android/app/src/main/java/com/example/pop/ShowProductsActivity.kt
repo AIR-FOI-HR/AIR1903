19527920ad0609ee0a279c0b86f077ecdd3fc638
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-12-06 13:30:26 +0100

    #62 #15 Provjera tokena kod dohvaćanja proizvoda
    Ako token nije ispravan, aplikacija se vraća na ekran za prijavu

--- Android/app/src/main/java/com/example/pop/ShowProductsActivity.kt
+++ Android/app/src/main/java/com/example/pop/ShowProductsActivity.kt
@@ -1,15 +1,18 @@
 package com.example.pop
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
-import com.example.webservice.Model.ProductResponse
 import com.example.pop.adapters.ProductRecyclerAdapter
+import com.example.pop_sajamv2.Session
 import com.example.webservice.Common.Common
+import com.example.webservice.Model.ProductResponse
 import kotlinx.android.synthetic.main.activity_show_products.*
-import retrofit2.*
-import kotlinx.android.synthetic.main.product_list_item.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ShowProductsActivity : AppCompatActivity(){
     private lateinit var productAdapter: ProductRecyclerAdapter
@@ -24,15 +27,22 @@ class ShowProductsActivity : AppCompatActivity(){
         product_list.adapter = productAdapter
 
         var productsApi = Common.api
-
-        productsApi.getProducts().enqueue(object : Callback<ProductResponse> {
+        productsApi.getProducts(true, Session.user.Token).enqueue(object : Callback<ProductResponse> {
             override fun onFailure(call: Call<ProductResponse>, t: Throwable) {
                 Toast.makeText(this@ShowProductsActivity, t!!.message, Toast.LENGTH_SHORT).show()
             }
 
             override fun onResponse(call: Call<ProductResponse>, response: Response<ProductResponse>) {
-                var resp = response!!.body()!!.DATA!!
-                productAdapter.submitList(resp)
+                var resp = response!!.body()!!.DATA
+                println(Session.user.Token)
+                if (response!!.body()!!.STATUSMESSAGE=="OLD TOKEN"){
+                    var intent = Intent(this@ShowProductsActivity, LoginActivity::class.java)
+                    Toast.makeText(this@ShowProductsActivity, "Sesija istekla, molimo prijavite se ponovno", Toast.LENGTH_LONG).show()
+                    Session.reset()
+                    startActivity(intent)
+                }
+                if (resp!=null)
+                    productAdapter.submitList(resp)
             }
         })
     }
