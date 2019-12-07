3605825d8abd6a5297b50370babd06a7cf30c38f
dbaban <dbaban@foi.hr>
2019-12-07 07:11:31 +0100

    #14, #39 Dodana poveznica na aktivnost za upravljanje proizvodima
    Dodane su poveznice sa Main Menu izbornika na aktivnosti za uređivanje i dodavenje proizvoda

--- Android/app/src/main/java/com/example/pop/MainMenu.kt
+++ Android/app/src/main/java/com/example/pop/MainMenu.kt
@@ -3,10 +3,9 @@ package com.example.pop
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.pop.R
 import com.example.webservice.Common.Common
+import com.example.webservice.Model.Product
 import com.example.webservice.Response.IMyAPI
-import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main_menu.*
 
 class MainMenu : AppCompatActivity() {
@@ -19,13 +18,22 @@ class MainMenu : AppCompatActivity() {
 
         showAllProductsButton.setOnClickListener{showProducts()}
         addProductButton.setOnClickListener{addNewProduct()}
+        editProductButton.setOnClickListener { editProduct() }
     }
     private fun showProducts(){
         val intent = Intent(this, ShowProductsActivity::class.java)
         startActivity(intent)
     }
     private fun addNewProduct(){
-        val intent = Intent(this, AddNewProduct::class.java)
+        val intent = Intent(this, ManageProductsActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun editProduct(){
+        val testProduct : Product = Product(1234, "Foi Product", 14.0, "Test product", "")
+
+        val intent = Intent(this, ManageProductsActivity::class.java)
+        intent.putExtra("product", testProduct)
         startActivity(intent)
     }
 }
