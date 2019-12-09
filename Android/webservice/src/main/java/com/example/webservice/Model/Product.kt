dc8ebefd2601aea1e7861b30a5429108a8d5e4c9
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-12-09 22:27:25 +0100

    Izmijenjene klase Product i NewProduct
    Dodana količina

--- Android/webservice/src/main/java/com/example/webservice/Model/Product.kt
+++ Android/webservice/src/main/java/com/example/webservice/Model/Product.kt
@@ -2,7 +2,7 @@ package com.example.webservice.Model
 
 import java.io.Serializable
 
-class Product(val Id: Int, var Naziv: String, var Cijena: Double, var Opis: String?, var Slika: String) : Serializable {
+class Product(val Id: Int, var Naziv: String, var Cijena: Double, var Opis: String?, var Slika: String, var Kolicina:String="") : Serializable {
     var isExpanded: Boolean = false
     var isSelected: Boolean = false
 }
@@ -11,4 +11,5 @@ class NewProduct{
     var Cijena:String=""
     var Opis:String=""
     var Slika:String=""
+    var Kolicina:String=""
 }
