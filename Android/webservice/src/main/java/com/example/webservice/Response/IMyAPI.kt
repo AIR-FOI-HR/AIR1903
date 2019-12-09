bbfd6150f2034f3540c31957e3064fb76eaae72b
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-12-09 22:26:45 +0100

    #45 #61 Izmjena call funkcija za uređivanje proizvoda
    Sad postoje dvije call funkcije, ako postoji slika, i ako ne postoji

--- Android/webservice/src/main/java/com/example/webservice/Response/IMyAPI.kt
+++ Android/webservice/src/main/java/com/example/webservice/Response/IMyAPI.kt
@@ -31,7 +31,11 @@ interface IMyAPI {
 
     @FormUrlEncoded
     @POST("proizvodi.php")
-    fun editProduct(@Field("Token")Token: String, @Field("Id")Id: Int, @Field("Naziv")Naziv: String, @Field("Opis") Opis: String, @Field("Cijena") Cijena: String?, @Field("Slika")Slika: String): Call<NewProductResponse>
+    fun editProductNoImage(@Field("Edit") Edit: Boolean, @Field("Token")Token: String, @Field("Id")Id: Int, @Field("Naziv")Naziv: String, @Field("Opis") Opis: String, @Field("Cijena") Cijena: String?, @Field("Kolicina") Kolicina:Int , @Field("Slika")Slika: String , @Field("KorisnickoIme") KorisnickoIme: String): Call<NewProductResponse>
+
+    @Multipart
+    @POST("proizvodi.php")
+    fun editProduct(@Part Edit:MultipartBody.Part, @Part Token:MultipartBody.Part, @Part Id:MultipartBody.Part, @Part Naziv:MultipartBody.Part, @Part Opis:MultipartBody.Part, @Part Cijena:MultipartBody.Part, @Part Kolicina:MultipartBody.Part, @Part file: MultipartBody.Part, @Part KorisnickoIme: MultipartBody.Part): Call<NewProductResponse>
 
     @FormUrlEncoded
     @POST("proizvodi.php")
