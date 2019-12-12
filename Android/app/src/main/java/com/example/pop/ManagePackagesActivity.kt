aee73a85d4efceeb5df001054a67f98f48245dea
dbaban <dbaban@foi.hr>
2019-12-12 16:59:58 +0100

    #19, #40 Promjena izgleda sučelja za upravljanja paketima

--- Android/app/src/main/java/com/example/pop/ManagePackagesActivity.kt
+++ Android/app/src/main/java/com/example/pop/ManagePackagesActivity.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.webservice.Model.Product
+import kotlinx.android.synthetic.main.activity_manage_packages.*
+
 
 class ManagePackagesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage_packages)
+
+        if(intent.hasExtra("package")) {
+            if(intent.getSerializableExtra("package") != null) {
+                
+            }
+        }
     }
 }
