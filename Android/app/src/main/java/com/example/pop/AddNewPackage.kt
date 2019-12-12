6c35c1a9c637e3f372ff972d858256f8da1ffda8
Josip Fuduric <josip0000007@gmail.com>
2019-12-12 07:39:23 +0100

    #37 Izmjena klase AddNewPackag.
    Dodani pojedini importovi, polje popust, te dvije varijable koje će nam kasnije trebati za spinner (drop-down meni).

--- Android/app/src/main/java/com/example/pop/AddNewPackage.kt
+++ Android/app/src/main/java/com/example/pop/AddNewPackage.kt
@@ -2,8 +2,18 @@ package com.example.pop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Spinner
+import com.example.webservice.Response.IMyAPI
 
 class AddNewPackage : AppCompatActivity() {
+
+    internal lateinit var mService: IMyAPI
+
+    var discounts = arrayOf("10", "20", "30" , "25", "15")
+
+    lateinit var discountForDatabase : String
+
+    lateinit var spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
