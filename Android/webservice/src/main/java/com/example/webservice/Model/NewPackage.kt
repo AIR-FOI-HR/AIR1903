3740e4da3f235d111f502cd3a5147e0b0df6f05c
Josip Fuduric <josip0000007@gmail.com>
2019-12-12 07:08:53 +0100

    #66 Dodana klasa NewPackageReponse.
    Dodana nova klasa NewPackageResponse, te pojedini nedostajali importovi.

--- Android/webservice/src/main/java/com/example/webservice/Model/NewPackage.kt
+++ Android/webservice/src/main/java/com/example/webservice/Model/NewPackage.kt
@@ -2,6 +2,13 @@ package com.example.webservice.Model
 
 import java.io.Serializable
 
+class NewPackage {
+    var NazivPaketa:String=""
+    var Id_Proizvoda:String=""
+    var Kolicina:String=""
+    var Popust:String=""
+}
+
 class Package(val Id: Int, var NazivPaketa: String, var Id_Proizvoda: String, var Popust: String, var Kolicina:String="") :
     Serializable {
     var isSelected: Boolean = false
---
+++ Android/webservice/src/main/java/com/example/webservice/Model/NewPackageResponse.kt
@@ -0,0 +1,7 @@
+package com.example.webservice.Model
+
+class NewPackageResponse {
+    var STATUS:Boolean=false
+    var STATUSMESSAGE:String?=null
+    var DATA: NewPackage?=null
+}
