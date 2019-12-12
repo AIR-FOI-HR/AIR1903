cbd8bcce32f853f93305a1a23d43fda6dda7df87
Josip Fuduric <josip0000007@gmail.com>
2019-12-12 07:25:16 +0100

    #37 Izmjena klase NewPackage.
    Dodani sve potrebne varijable.

--- Android/webservice/src/main/java/com/example/webservice/Model/NewPackage.kt
+++ Android/webservice/src/main/java/com/example/webservice/Model/NewPackage.kt
@@ -3,5 +3,8 @@ package com.example.webservice.Model
 import java.io.Serializable
 
 class NewPackage {
-
+    var NazivPaketa:String=""
+    var Id_Proizvoda:String=""
+    var Kolicina:String=""
+    var Popust:String=""
 }
