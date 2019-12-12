9d4b952f2e067c1755c33bf29dd76f575d0b82bd
Josip Fuduric <josip0000007@gmail.com>
2019-12-12 07:31:29 +0100

    #37 Kreirana nova aktivnost AddNewPackage.
    Aktivnost će služiti za dodavanje novoga paketa.

---
+++ Android/app/src/main/java/com/example/pop/AddNewPackage.kt
@@ -0,0 +1,12 @@
+package com.example.pop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNewPackage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_package)
+    }
+}
