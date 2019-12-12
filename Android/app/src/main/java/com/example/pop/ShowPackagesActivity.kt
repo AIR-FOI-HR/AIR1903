b4927d6596b776d5cbc243b7d431bd0fc2a45d80
dbaban <dbaban@foi.hr>
2019-12-12 16:57:46 +0100

    #20 Napravljeno sučelje za pregled paketa
    Napravljena je osnovna struktura sučelja kao i poveznica na zaslon za dodavanje novog paketa

--- Android/app/src/main/java/com/example/pop/ShowPackagesActivity.kt
+++ Android/app/src/main/java/com/example/pop/ShowPackagesActivity.kt
@@ -1,11 +1,20 @@
 package com.example.pop
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_show_packages.*
 
 class ShowPackagesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        layoutShowPackagesButtonNewPackage.setOnClickListener { createPackage() }
+    }
+
+    private fun createPackage() {
+        val intent = Intent(applicationContext, ManagePackagesActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        applicationContext.startActivity(intent)
     }
 }
