6bc91fcdc6dbe4a41a058a67cf70872a00623de3
Josip Fuduric <josip0000007@gmail.com>
2019-12-12 07:04:31 +0100

    #66 Izmjena klase Package.
    Klasa popunjena ispravnim parametrima.

--- Android/webservice/src/main/java/com/example/webservice/Model/NewPackage.kt
+++ Android/webservice/src/main/java/com/example/webservice/Model/NewPackage.kt
@@ -1,6 +1,8 @@
 package com.example.webservice.Model
 
+import java.io.Serializable
 
-
-class Package(
+class Package(val Id: Int, var NazivPaketa: String, var Id_Proizvoda: String, var Popust: String, var Kolicina:String="") :
+    Serializable {
+    var isSelected: Boolean = false
 }
