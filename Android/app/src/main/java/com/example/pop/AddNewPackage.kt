32769fb69181c22f886d6172e43b0be6157d78f9
Josip Fuduric <josip0000007@gmail.com>
2019-12-12 07:40:46 +0100

    #Izmjena klase AddNewPackage.
    Dodana funkcija addNewPackage, te pojedini importovi.

--- Android/app/src/main/java/com/example/pop/AddNewPackage.kt
+++ Android/app/src/main/java/com/example/pop/AddNewPackage.kt
@@ -3,7 +3,14 @@ package com.example.pop
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Spinner
+import android.widget.Toast
+import com.example.pop_sajamv2.Session
+import com.example.webservice.Common.Common
+import com.example.webservice.Model.NewPackageResponse
 import com.example.webservice.Response.IMyAPI
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class AddNewPackage : AppCompatActivity() {
 
@@ -18,5 +25,11 @@ class AddNewPackage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new_package)
+
+        mService = Common.api
+    }
+    private fun addNewPackage(NazivPaketa: String, Id_Proizvoda: String, Kolicina: String, Popust: String) {
+        
+
     }
 }
