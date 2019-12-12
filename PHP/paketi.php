e4f7b701029a510179ef85923b6597b9394f06cd
Josip Fuduric <josip0000007@gmail.com>
2019-12-12 06:46:08 +0100

    #65 Izmjena skripte paketi.php.
    Skripta sada u slučaju da primi GET parametar vraća pakete pojedinog korisnika obzirom na njegovu ulogu.

--- PHP/paketi.php
+++ PHP/paketi.php
@@ -2,4 +2,19 @@
 require_once 'db_function.php';
 $db = new DB_Functions();
 header('Content-Type: application/json');
+if (isset($_POST["GET"]) && $_POST["GET"] == true) {
+        $allPackeges = $db->getAllPackeges($_POST);
+        if ($allPackeges[0] == 1) {
+            $response->DATA = null;
+            $response->STATUS = false;
+            $response->STATUSMESSAGE = "REGULAR USERS CAN'T READ";
+            $response = json_encode($response, JSON_UNESCAPED_UNICODE);
+            echo $response;
+        } else {
+            $response->DATA = $allPackeges[1];
+            $response->STATUS = true;
+            $response->STATUSMESSAGE = "OK";
+            $response = json_encode($response, JSON_UNESCAPED_UNICODE);
+            echo $response;
+        }
 ?>
