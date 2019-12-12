8c608383628b21e2b51ab60722322f8838ba1ba0
dbaban <dbaban@foi.hr>
2019-12-12 15:20:45 +0100

    #19, #20, #40 Dodani resursi zaslona za pregled paketa i upravljanje paketima

---
+++ Android/app/src/main/java/com/example/pop/ManagePackagesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pop
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ManagePackagesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ Android/app/src/main/java/com/example/pop/ShowPackagesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pop
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShowPackagesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
