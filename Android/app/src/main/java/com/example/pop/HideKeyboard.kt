9c4a41943c1d22eebf83e059591658e182643168
Mattheeeew <44161476+Mattheeeew@users.noreply.github.com>
2019-12-15 18:02:53 +0100

    Implemented additional method in HideKeyboard class to hide keyboard in fragments.

--- Android/app/src/main/java/com/example/pop/HideKeyboard.kt
+++ Android/app/src/main/java/com/example/pop/HideKeyboard.kt
@@ -1,10 +1,13 @@
 package com.example.pop
 
 import android.app.Activity
+import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 class HideKeyboard {
+
+    //hide keyboard in activity
     fun hideKeyboard(activity: Activity) {
         val imm: InputMethodManager =
             activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -15,4 +18,10 @@ class HideKeyboard {
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0)
     }
 
+    //hide keyboard in fragment
+    fun hideKeyboard(view: View) {
+        val inputManager = view.context
+            .getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 }
