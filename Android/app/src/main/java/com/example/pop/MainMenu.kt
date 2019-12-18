fafb7a0db48301864a6f857b3a3c6844ac9cd81f
Mattheeeew <44161476+Mattheeeew@users.noreply.github.com>
2019-12-18 19:41:19 +0100

    Dodan identifikator kartici glavnog izbornika za testiranje prikaza fragmenta unutar TabLayout-a. Kreirana funkcija showLayout za prikaz TabLayout aktivnosti unutar MainMenu klase.

--- Android/app/src/main/java/com/example/pop/MainMenu.kt
+++ Android/app/src/main/java/com/example/pop/MainMenu.kt
@@ -3,6 +3,7 @@ package com.example.pop
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
 import com.example.webservice.Common.Common
 import com.example.webservice.Model.Product
 import com.example.webservice.Response.IMyAPI
@@ -23,11 +24,19 @@ class MainMenu : AppCompatActivity() {
         addProductButton.setOnClickListener{addNewProduct()}
         editProductButton.setOnClickListener { editProduct() }
         showAllPackages.setOnClickListener { showPackages() }
+
+        tabLayout.setOnClickListener{showLayout()}
     }
     private fun showProducts(){
         val intent = Intent(this, ShowProductsActivity::class.java)
         startActivity(intent)
     }
+
+    private fun showLayout(){
+        val intent = Intent(this, TabLayoutActivity::class.java)
+        startActivity(intent)
+    }
+
     private fun addNewProduct(){
         val intent = Intent(this, ManageProductsActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
