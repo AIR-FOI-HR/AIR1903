0be1d59585282ca42fb4099a2af80d6f3efe4a0a
Mattheeeew <44161476+Mattheeeew@users.noreply.github.com>
2019-12-18 19:38:33 +0100

    Dodana dva fragmenta, ProductsTabFragment i PackagesTabFragment.

---
+++ Android/app/src/main/java/com/example/pop/PackagesTabFragment.kt
@@ -0,0 +1,24 @@
+package com.example.pop
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PackagesTabFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_packages_tab, container, false)
+    }
+
+
+}
