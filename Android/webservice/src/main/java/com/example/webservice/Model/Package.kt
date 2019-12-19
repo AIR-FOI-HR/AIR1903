252de265c644659b06842b4f150a65687e4b507c
aalagic <aldin_alagic@hotmail.com>
2019-12-19 11:49:03 +0100

    no message

--- Android/webservice/src/main/java/com/example/webservice/Model/Package.kt
+++ Android/webservice/src/main/java/com/example/webservice/Model/Package.kt
@@ -3,6 +3,7 @@ package com.example.webservice.Model
 class Package(
     var Popust: Double = 0.00,
     var KolicinaPaketa: Int = 0,
+    var Kolicina: Int = 0,
     override val Id: Int?,
     override var Naziv: String = "",
     override var Opis: String? = "",
@@ -10,3 +11,4 @@ class Package(
     override var expanded: Boolean = false,
     override var selected: Boolean = false
 ) : Item
+
