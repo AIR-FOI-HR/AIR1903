2782c69bef273adc3b6398600160adfcd04374f0
aalagic <aldin_alagic@hotmail.com>
2019-12-19 11:50:21 +0100

    Pretvorba activity u fragmente kod Pacakges i Products sučelja
    Pretvorena sučelja za prikaz proizvoda i paketa iz activity oblika u fragment, kako bi se prikazivali u pageview.

--- Android/app/src/main/java/com/example/pop/fragments/PackagesFragment.kt
+++ Android/app/src/main/java/com/example/pop/fragments/PackagesFragment.kt
@@ -1,17 +1,32 @@
 package com.example.pop.fragments
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.pop.LoginActivity
+import com.example.pop.ManagePackagesActivity
+import com.example.pop.ManageProductsActivity
 import com.example.pop.R
+import com.example.pop.adapters.ItemRecyclerAdapter
+import com.example.pop_sajamv2.Session
+import com.example.webservice.Common.Common
+import com.example.webservice.Model.Item
+import com.example.webservice.Model.Package
+import com.example.webservice.Model.PackageResponse
+import kotlinx.android.synthetic.main.activity_show_products.*
+import kotlinx.android.synthetic.main.fragment_packages_tab.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.Serializable
 
-/**
- * A simple [Fragment] subclass.
- */
-class PackagesTabFragment : Fragment() {
+class PackagesFragment : Fragment() {
+    private lateinit var itemAdapter: ItemRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,5 +36,51 @@ class PackagesTabFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_packages_tab, container, false)
     }
 
+   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+            itemAdapter = ItemRecyclerAdapter(context)
+            package_list.adapter = itemAdapter
+            getPackages()
+
+            btn_new_package.setOnClickListener{addPackage()}
+        }
+
+
+
+        private fun getPackages(){
+            val api = Common.api
+            api.getAllPackages(Session.user.Token, true, Session.user.KorisnickoIme).enqueue(object :
+                Callback<PackageResponse> {
+                override fun onFailure(call: Call<PackageResponse>, t: Throwable) {
+                    Toast.makeText(context, t.message, Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onResponse(call: Call<PackageResponse>, response: Response<PackageResponse>) {
+                    val resp = response.body()!!.DATA
+
+                    when {
+                        response.body()!!.STATUSMESSAGE=="OLD TOKEN" -> {
+                            val intent = Intent(activity, LoginActivity::class.java)
+                            Toast.makeText(context, "Sesija istekla, molimo prijavite se ponovno", Toast.LENGTH_LONG).show()
+                            Session.reset()
+                            startActivity(intent)
+                            activity?.finish()
+                        }
+                        response.body()!!.STATUSMESSAGE=="OK" -> {}
+                        else -> Toast.makeText(context, response.body()!!.STATUSMESSAGE, Toast.LENGTH_LONG).show()
+                    }
+
+                    if (resp != null) itemAdapter.submitList(resp)
+                }
+            })
+        }
+
+        private fun addPackage(){
+            val intent= Intent(context, ManageProductsActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.putExtra("previousActivity", 1)
+            context?.startActivity(intent)
+        }
 
 }
--- Android/app/src/main/java/com/example/pop/fragments/ProductsFragment.kt
+++ Android/app/src/main/java/com/example/pop/fragments/ProductsFragment.kt
@@ -1,19 +1,28 @@
 package com.example.pop.fragments
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.pop.R
-import com.example.pop.adapters.ProductRecyclerAdapter
+import android.widget.Toast
+import com.example.pop.*
+import com.example.pop.adapters.ItemRecyclerAdapter
+import com.example.pop_sajamv2.Session
+import com.example.webservice.Common.Common
+import com.example.webservice.Model.Item
+import com.example.webservice.Model.ProductResponse
+import kotlinx.android.synthetic.main.activity_show_products.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.Serializable
 
-/**
- * A simple [Fragment] subclass.
- */
-class ProductsTabFragment : Fragment() {
-    private lateinit var productAdapter: ProductRecyclerAdapter
+class ProductsFragment : Fragment() {
+    private lateinit var itemAdapter: ItemRecyclerAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,4 +30,63 @@ class ProductsTabFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_products_tab, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        itemAdapter = ItemRecyclerAdapter(context)
+        product_list.adapter = itemAdapter
+        getProducts()
+
+        btn_new_product.setOnClickListener{addProduct()}
+        btn_new_package.setOnClickListener{createPackage()}
+    }
+
+
+
+    private fun getProducts(){
+        val api = Common.api
+        api.getProducts(true, Session.user.Token, Session.user.KorisnickoIme).enqueue(object :
+            Callback<ProductResponse> {
+            override fun onFailure(call: Call<ProductResponse>, t: Throwable) {
+                Toast.makeText(context, t.message, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(call: Call<ProductResponse>, response: Response<ProductResponse>) {
+                val resp = response.body()!!.DATA
+
+                when {
+                    response.body()!!.STATUSMESSAGE=="OLD TOKEN" -> {
+                        val intent = Intent(activity, LoginActivity::class.java)
+                        Toast.makeText(context, "Sesija istekla, molimo prijavite se ponovno", Toast.LENGTH_LONG).show()
+                        Session.reset()
+                        startActivity(intent)
+                        activity?.finish()
+                    }
+                    response.body()!!.STATUSMESSAGE=="OK" -> {}
+                    else -> Toast.makeText(context, response.body()!!.STATUSMESSAGE, Toast.LENGTH_LONG).show()
+                }
+
+                if (resp != null) itemAdapter.submitList(resp)
+            }
+        })
+    }
+
+    private fun addProduct(){
+        val intent= Intent(context, ManageProductsActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.putExtra("previousActivity", 1)
+        context?.startActivity(intent)
+    }
+
+    private fun createPackage() {
+        val selectedProducts : List<Item> = itemAdapter.getSelectedItems()
+        val intent = Intent(context, ManagePackagesActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.putExtra("productList", selectedProducts as Serializable)
+        context?.startActivity(intent)
+    }
+
+
+
 }
