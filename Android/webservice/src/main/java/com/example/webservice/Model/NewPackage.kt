d9a4ea5eb5129901e9492f833ad18c8b040ab2d8
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-12-19 02:29:25 +0100

    #22 #69 #70 Uređene klase odgovora servera za pakete

--- Android/webservice/src/main/java/com/example/webservice/Model/NewPackage.kt
+++ Android/webservice/src/main/java/com/example/webservice/Model/NewPackage.kt
@@ -3,12 +3,13 @@ package com.example.webservice.Model
 import java.io.Serializable
 
 class NewPackage {
-    var NazivPaketa:String=""
-    var Id_Proizvoda:String=""
+    var Naziv:String=""
+    var Opis:String=""
+    var Slika:String=""
     var Kolicina:String=""
     var Popust:String=""
 }
-class Package(val Id: Int, var NazivPaketa: String, var Id_Proizvoda: String, var Popust: String, var Kolicina:String="") :
+class Package(val Id: Int, var Naziv: String, var Opis: String, var Popust: String, var Slika:String="") :
     Serializable {
     var isSelected: Boolean = false
 }
