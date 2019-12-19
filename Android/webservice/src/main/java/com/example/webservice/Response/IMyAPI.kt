c50617b1b0523677f5a30bac3cfe2c819ca0ea10
Josip Fuduric <josip0000007@gmail.com>
2019-12-19 10:03:47 +0100

    #69 Izmjena interface-a IMyAPI.
    Dodana funkcija updatePackageWithImage, koja će služiti za izmjenu paketa sa slikom.

--- Android/webservice/src/main/java/com/example/webservice/Response/IMyAPI.kt
+++ Android/webservice/src/main/java/com/example/webservice/Response/IMyAPI.kt
@@ -49,7 +49,7 @@ interface IMyAPI {
 
     @Multipart
     @POST("paketi.php")
-    fun addNewPackageWithImage(@Part Token:MultipartBody.Part, @Part Naziv:MultipartBody.Part, @Part Opis:MultipartBody.Part, @Part Popust:MultipartBody.Part, @Part Kolicina:MultipartBody.Part, @Part file: MultipartBody.Part, @Part KorisnickoIme: MultipartBody.Part, @Part KolicinaPaketa: MultipartBody.Part): Call<NewProductResponse>
+    fun addNewPackageWithImage(@Part Token:MultipartBody.Part, @Part ADD:MultipartBody.Part, @Part Naziv:MultipartBody.Part, @Part Opis:MultipartBody.Part, @Part Popust:MultipartBody.Part, @Part Kolicina:MultipartBody.Part, @Part file: MultipartBody.Part, @Part KorisnickoIme: MultipartBody.Part, @Part KolicinaPaketa: MultipartBody.Part): Call<NewProductResponse>
 
     @FormUrlEncoded
     @POST("paketi.php")
@@ -63,6 +63,10 @@ interface IMyAPI {
     @FormUrlEncoded
     @POST("paketi.php")
     fun updatePackage(@Field("Token") Token: String,@Field("UPDATE") UPDATE: Boolean, @Field("Id") Id: String, @Field("Naziv") Naziv: String, @Field("Opis") Opis: String, @Field("Kolicina") Kolicina: String, @Field("Popust") Popust: String) : Call<NewPackageResponse>
+
+    @Multipart
+    @POST("paketi.php")
+    fun updatePackageWithImage(@Part UPDATE:MultipartBody.Part, @Part Token:MultipartBody.Part, @Part Id:MultipartBody.Part, @Part Naziv:MultipartBody.Part, @Part Opis:MultipartBody.Part, @Part Popust:MultipartBody.Part, @Part Kolicina:MultipartBody.Part, @Part file: MultipartBody.Part): Call<NewProductResponse>
 
     @FormUrlEncoded
     @POST("paketi.php")
