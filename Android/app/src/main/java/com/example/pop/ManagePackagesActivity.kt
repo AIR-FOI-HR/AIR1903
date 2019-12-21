f6b46508252754511adc0f6fc3daa21bfb5de79d
aalagic <aldin_alagic@hotmail.com>
2019-12-22 00:45:54 +0100

    #40 #19 Napravljena navigacija između fragmenata sučelja za dodavanje i izmjenu paketa
    Povezani i dorađeni fragmenti na sučelju za dodavanje i izmjenu paketa.

--- Android/app/src/main/java/com/example/pop/ManagePackagesActivity.kt
+++ Android/app/src/main/java/com/example/pop/ManagePackagesActivity.kt
@@ -1,33 +1,14 @@
 package com.example.pop
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_manage_packages.*
 
 
 class ManagePackagesActivity : AppCompatActivity() {
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage_packages)
-
-        if(intent.hasExtra("package")) {
-            if(intent.getSerializableExtra("package") != null) {
-                
-            }
-        }
-
-        layoutManagePackagesButtonSubmit.setOnClickListener { submitPackage() }
     }
 
-
-
-    private fun submitPackage() {
-        val intent = Intent(applicationContext, ShowItemsActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        applicationContext.startActivity(intent)
-    }
 }
