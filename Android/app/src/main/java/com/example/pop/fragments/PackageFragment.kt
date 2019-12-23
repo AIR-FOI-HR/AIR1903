6f3ad2dd20fd2767f7b0a9d9b4db7f8b32067a34
Josip Fuduric <josip0000007@gmail.com>
2019-12-23 13:11:56 +0100

    #22 Izmjena funkcije addPackage.
    Funckija sada implementirana.

--- Android/app/src/main/java/com/example/pop/fragments/PackageFragment.kt
+++ Android/app/src/main/java/com/example/pop/fragments/PackageFragment.kt
@@ -1,11 +1,14 @@
 package com.example.pop.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.example.pop.ManagePackagesActivity
+import com.example.pop.ManageProductsActivity
 import com.example.pop.R
 import kotlinx.android.synthetic.main.fragment_package.view.*
 
@@ -28,6 +31,9 @@ class PackageFragment : Fragment() {
     }
 
     private fun addPackage() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val intent= Intent(context, ManagePackagesActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.putExtra("previousActivity", 1)
+        context?.startActivity(intent)
     }
 }
