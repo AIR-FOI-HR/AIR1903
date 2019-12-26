7c2e57fff0bee7effd1aee818b736e6f6eb972a4
Josip Fuduric <josip0000007@gmail.com>
2019-12-26 11:02:51 +0100

    #35 Izmjena funkcije novcanik.php.
    Dodana provjera da li je primljen GET parametar preko post zahtjeva, ukoliko jeste dohvaća se stanje novćanika pojedinog korisnika.

--- PHP/novcanik.php
+++ PHP/novcanik.php
@@ -5,6 +5,15 @@ $db = new DB_Functions();
 header('Content-Type: application/json');
 
 if ($db->checkAuth($_POST["Token"])) {
+    if (isset($_POST["GET"]) && $_POST["GET"] == true) {
+        $userBalance = $db->getBalance($_POST);
+
+        $response->STATUS = true;
+        $response->STATUSMESSAGE = "Stanje računa: ";
+        $response->DATA = $userBalance;
+        $response = json_encode($response, JSON_UNESCAPED_UNICODE);
+        echo $response;
+    }
     
 }
 ?>
