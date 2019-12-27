4cecfdf8764e6b8648518eb8ef3aa8dd41230a95
Mattheeeew <44161476+Mattheeeew@users.noreply.github.com>
2019-12-27 12:55:39 +0100

    #25 Kreirano sučelje za pregled stanja virtualnog novčanika.
    Kreiran ShowWalletBalanceActivity i dodan TextView za prikaz stanja unutar xml dokumenta.

---
+++ Android/app/src/main/java/com/example/pop/ShowWalletBalanceActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowWalletBalanceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_wallet_balance)
+    }
+}
