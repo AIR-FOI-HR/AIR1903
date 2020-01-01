9ac9ed42e743da02e9dc526cc7601c97ea5fe989
Josip Fuduric <josip0000007@gmail.com>
2020-01-01 14:31:12 +0100

    #38 Izmjena skripte db_functions.php.
    Dodana funckija sellItems.

--- PHP/db_functions.php
+++ PHP/db_functions.php
@@ -643,6 +643,10 @@ public function setInitialBalance($post) {
         
         return $response;
     }
+ //funkcija za smanjenje kolicine proizvoda prilikom prodaje, funkcija takoder mijenja stanje novcanika kupca i prodavaca
+public function sellItems($post) {
+      
+    }
 
 }
 ?>
