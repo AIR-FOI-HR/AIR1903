edbf8263869cf1c00b259604f440cffd4722cc95
dbaban <dbaban@foi.hr>
2020-01-08 17:52:57 +0100

    #2, #4 Dodan ViewHolder za elemente liste računa

--- Android/app/src/main/java/com/example/pop/ShowReceiptsActivity.kt
+++ Android/app/src/main/java/com/example/pop/ShowReceiptsActivity.kt
@@ -2,11 +2,15 @@ package com.example.pop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pop.adapters.ReceiptAdapter
 
 class ShowReceiptsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_receipts)
+
+        val adapter = ReceiptAdapter()
+        //adapter.data =
     }
 }
---
+++ Android/app/src/main/java/com/example/pop/adapters/ReceiptViewHolder.kt
@@ -0,0 +1,13 @@
+package com.example.pop.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.receipt_list_item.view.*
+
+class ReceiptViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(/*receipt: Receipt*/) {
+        //itemView.layoutReceiptItemTextName.text =
+        //itemView.layoutReceiptItemTextDate.text =
+        //itemView.layoutReceiptItemTextPrice.text =
+    }
+}
