2464d30f428a92a0acdc67a92ae49f5db24c5f55
Mattheeeew <44161476+Mattheeeew@users.noreply.github.com>
2020-01-15 19:51:03 +0100

    #29 Kreiran qr modul za plaćanje putem QR koda.
    Kreiran QR modul i pripadajući QRCode objekt sa funkcijom za generiranje QR kod koja prima ukupan iznos računa na temelju čega se generira QR.

--- Android/qr/src/main/java/com/example/qr/QRCode.kt
+++ Android/qr/src/main/java/com/example/qr/QRCode.kt
@@ -5,10 +5,30 @@ import android.graphics.Color
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.qrcode.QRCodeWriter
 
-class QRCode {
+
+object QRCode {
+    fun generateQRCode(amount : String): Bitmap{
+        val content = amount
+        val writer = QRCodeWriter()
+        val bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, 1200, 1200)
+        val width = bitMatrix.width
+        val height = bitMatrix.height
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                bitmap.setPixel(x, y, if (bitMatrix.get(x, y)) Color.BLACK else Color.WHITE)
+            }
+        }
+
+        return bitmap
+
+        //set image bitmap
+        //imageView.setImageBitmap(bitmap)
+    }
 }
 
-private fun generateQRCode(){
+/*
+public fun generateQRCode(){
 
     //QR Code content -> iznos racuna za naplatiti
     val content = ""
@@ -29,3 +49,4 @@ private fun generateQRCode(){
     //set image bitmap
     //imageView.setImageBitmap(bitmap)
 }
+ */
