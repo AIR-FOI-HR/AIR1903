07d116bd62ec07128f6ea15cb3e53943c3cd2ac4
dbaban <dbaban@foi.hr>
2020-01-15 12:23:45 +0100

    #74 Dodana funkcionalnost računanja cijene pojedinog artikla
    Dodana valuta na layout

--- Android/app/src/main/java/com/example/pop/adapters/SellItemsViewHolder.kt
+++ Android/app/src/main/java/com/example/pop/adapters/SellItemsViewHolder.kt
@@ -7,16 +7,26 @@ import com.example.webservice.Model.PackageClass
 import com.example.webservice.Model.Product
 import kotlinx.android.synthetic.main.sell_item_list.view.*
 
+const val INITIAL_QUANTITY = 1
+
 class SellItemsViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+    private var quantityLimit : Int = 10000
     fun bind(item : Item) {
         itemView.layoutSellItemListName.text = item.Naziv
+        itemView.layoutSellItemListQuantity.text = INITIAL_QUANTITY.toString()
 
-        //Ovo treba sredit
-        if(item is PackageClass) itemView.layoutSellItemListQuantity.text = item.Kolicina
-        if(item is Product) itemView.layoutSellItemListQuantity.text = item.Kolicina
+        if(item is PackageClass) quantityLimit = item.Kolicina!!.toInt()
+        else if(item is Product) quantityLimit = item.Kolicina.toInt()
+        updatePrice(item)
 
-        itemView.layoutSellItemListButtonDecrease.setOnClickListener { decreaseQuantity(itemView) }
-        itemView.layoutSellItemListButtonIncrease.setOnClickListener { increaseQuantity(itemView) }
+        itemView.layoutSellItemListButtonDecrease.setOnClickListener {
+            decreaseQuantity(itemView)
+            updatePrice(item)
+        }
+        itemView.layoutSellItemListButtonIncrease.setOnClickListener {
+            increaseQuantity(itemView)
+            updatePrice(item)
+        }
     }
 
     private fun decreaseQuantity(view : View) {
@@ -27,7 +37,16 @@ class SellItemsViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
 
     private fun increaseQuantity(view: View) {
         var quantity : Int = view.layoutSellItemListQuantity.text.toString().toInt()
-        if(quantity < 100000) quantity += 1
+        if(quantity < quantityLimit) quantity += 1
         view.layoutSellItemListQuantity.text = quantity.toString()
+    }
+
+    private fun updatePrice(item : Item) {
+        if(item is PackageClass) {
+            itemView.layoutSellItemListPrice.text = item.Popust //Tu treba biti cjena paketa
+        }
+        else if(item is Product) {
+            itemView.layoutSellItemListPrice.text = (item.Cijena.toInt() * itemView.layoutSellItemListQuantity.text.toString().toInt()).toString()
+        }
     }
 }
