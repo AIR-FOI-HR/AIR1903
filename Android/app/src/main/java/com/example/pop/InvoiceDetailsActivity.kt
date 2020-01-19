8d87b7067e07d71ed535bf6959e22522ecded70b
dbaban <dbaban@foi.hr>
2020-01-19 16:26:59 +0100

    #1 Napravljena aktivnost za prikaz detalja računa

---
+++ Android/app/src/main/java/com/example/pop/InvoiceDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InvoiceDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_invoice_details)
+    }
+}
