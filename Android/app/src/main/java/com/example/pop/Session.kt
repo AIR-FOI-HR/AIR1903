923e713be1488b33b6bbb82d6ce8ffc5e0b8db57
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2020-01-21 23:05:01 +0100

    #3 Kod generiranja QR koda, ID računa se proširuje
    Neki mobilni uređaji imaju problem s malim QR kodovima, pa se ID množi s unaprijed definiranim velikim brojem, i takav se šalje

--- Android/app/src/main/java/com/example/pop/Session.kt
+++ Android/app/src/main/java/com/example/pop/Session.kt
@@ -1,9 +1,11 @@
 package com.example.pop_sajamv2
 
 import com.example.webservice.Model.User
+import java.math.BigInteger
 
 object Session {
     var user:User=User()
+    val expander = BigInteger("10366590174751098504021101516526571562453730560926949855400199801242584769764638222759018763045057889411844305742024015554773846491506097706516145168109883")
     fun reset(){
         user.reset()
     }
