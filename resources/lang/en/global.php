86f37b79e1304a08b52e329d424b7a9d843106d8
aalagic <aldin_alagic@hotmail.com>
2020-02-20 00:42:59 +0100

    #84 Dorađen dizajn sučelja za prijavu administratora
    Napravljene izmjene dizajna boja sučelja za prijavu administratora.

---
+++ resources/lang/en/global.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'delete' => 'Delete',
+	'close' => 'Close',
+	'showentries' => 'Show _MENU_',
+    'noresult' => 'No results',
+    'next' => 'Next',
+    'previous' => 'Previous',
+    'search' => 'Search',
+];
