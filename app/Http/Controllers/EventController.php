7804219178758ea196ae0512140c08c3fcb34291
aalagic <aldin_alagic@hotmail.com>
2020-02-23 17:37:59 +0100

    #95 Napravljen controller za implementaciju funkcionalnosti vezane za događaje
    Napravljen kontroler koji omogućava funkcionalnosti vezane za događaje: dohvat, dodavanje, brisanje i izmjenu događaja.

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class EventController extends Controller
+{
+    public function index()
+    {
+        $_POST['Token'] = session('token');
+        $_POST['KorisnickoIme'] = session('korisnickoIme');
+        $_POST['Readall'] = 'true';
+
+        $ch = curl_init("http://cortex.foi.hr/pop/meta.php");
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = json_decode(curl_exec($ch), true);
+        $events = $result['DATA'];
+        curl_close($ch);
+
+        return view('events.index', compact('events'));
+    }
+
+    public function create()
+    {
+        return view('events.create');
+    }
+    
+    public function store()
+    {
+        $_POST['Token'] = session('token');
+        $_POST['KorisnickoIme'] = session('korisnickoIme');
+        $_POST['CREATEEVENT'] = 'true';
+        $_POST['NazivEventa'] = request()->input('Naziv');
+
+        $ch = curl_init("http://cortex.foi.hr/pop/meta.php");
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = json_decode(curl_exec($ch), true);
+        curl_close($ch);
+        
+        if($result['STATUSMESSAGE'] == "SUCCESS" )
+            session()->flash('success', "Uspješno dodan novi događaj.");
+        else
+            session()->flash('error', "Greška prilikom dodavanja događaja.");
+
+        return redirect()->route('events.index');
+    }
+
+    public function show($id)
+    {
+        $event = request()->all();
+        $event['Id'] = $id;
+        return view('events.show', compact('event'));
+    }
+
+    public function edit($id)
+    {
+        $event = request()->all();
+        $event['Id'] = $id;
+        return view('events.edit', compact('event'));
+    }
+
+    public function update()
+    {
+        $_POST['Token'] = session('token');
+        $_POST['KorisnickoIme'] = session('korisnickoIme');
+        $_POST['EDITEVENT'] = 'true';
+        $_POST['Naziv_Eventa'] = request()->input('Naziv');
+        $_POST['Id_Eventa'] = request()->input('Id');
+
+        $ch = curl_init("http://cortex.foi.hr/pop/meta.php");
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = json_decode(curl_exec($ch), true);
+        curl_close($ch);
+        
+        if($result['STATUSMESSAGE'] == "EVENT EDITED" )
+            session()->flash('success', "Uspješno izmijenjen događaj.");
+        else
+            session()->flash('error', "Greška prilikom izmjene događaja.");
+
+        return redirect()->route('events.index');
+    }
+
+    public function updateStatus($event)
+    {
+        $_POST['Token'] = session('token');
+        $_POST['KorisnickoIme'] = session('korisnickoIme');
+        $_POST['SETCURRENTEVENT'] = 'true';
+        $_POST['Id_Eventa'] = $event;
+
+        $ch = curl_init("http://cortex.foi.hr/pop/meta.php");
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = json_decode(curl_exec($ch), true);
+        curl_close($ch);
+
+        if($result['STATUSMESSAGE'] == "SUCCESS")
+            session()->flash('success', "Događaj je uspješno aktiviran.");
+        else
+            session()->flash('error', "Greška prilikom aktiviranja događaja");
+        
+        return redirect()->route('events.index');
+    }
+
+    public function destroy($event)
+    {
+       
+    }
+}
