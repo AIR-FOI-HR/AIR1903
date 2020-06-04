99663d5e46517711101ba01c15f648e8300a6c17
Zlatko S <zlatko.stapic@foi.hr>
2020-06-04 20:51:04 +0200

    Izmijenjen naziv klase.

--- PHP/db_connect.php
+++ PHP/db_connect.php
@@ -3,7 +3,7 @@ class DB_CONNECT{
     private $conn;
     public function connect() {
         require_once 'dbConfig.php';
-        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+        $this->conn = new MySQLi(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
         return $this->conn;
         
     }
